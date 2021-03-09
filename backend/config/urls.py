15fe344e9932389df09e6219d2b1ae030cfd1219
Aquinology <riabova_e@iuca.kg>
2021-03-09 17:16:07 +0600

    auth

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from .router import router
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
+    path('auth/', include('auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
